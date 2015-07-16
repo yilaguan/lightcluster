f69581389cad70a57a08f5402af6bb8b64f8e291
igorsilin <siliniv@gmail.com>
2015-07-16 13:38:33 +0300

    test

--- load_data.py
+++ load_data.py
@@ -1,1 +1,25 @@
 # wrapper for easy downloading data
+
+def download_data(filename):
+  f = open(filename,"r")
+  s = f.readline()
+  n_vertex, n_edge = s.split(' ',2);
+  n_vertex = int(n_vertex)
+  n_edge = int(n_edge)
+  edgelist = [];
+  for i in range(n_edge):
+    s = f.readline()
+    vertex1, vertex2 = s.split(' ',2);
+    vertex1 = int(vertex1)-1
+    vertex2 = int(vertex2)-1
+    edgelist.append([vertex1, vertex2, 1])       
+  return [n_vertex, edgelist]
+
+def download_answer(filename):
+  f = open(filename,"r")
+  labels = [];
+  s = f.readline()
+  while s:
+    labels.append(int(s));
+    s = f.readline()
+  return labels
